a60718c86c165cebfbcd701c896cc447a79eb05b
arkneca <stojanpro@gmail.com>
2016-11-16 12:54:14 +0100

    Complete roman numerals

--- roman_numerals/lib/roman_numerals.rb
+++ roman_numerals/lib/roman_numerals.rb
@@ -1,3 +1,24 @@
-class RomanNumerals
+module RomanNumerals
+  ROMAN_NUMERALS = { 1000 => 'M',
+                     900  => 'CM',
+                     500  => 'D',
+                     400  => 'CD',
+                     100  => 'C',
+                     90   => 'XC',
+                     50   => 'L',
+                     40   => 'XL',
+                     10   => 'X',
+                     9    => 'IX',
+                     5    => 'V',
+                     4    => 'IV',
+                     1    => 'I' }.freeze
 
+  def self.convert(arabic_number)
+    roman_numeral = ""
+    ROMAN_NUMERALS.each do |number, letter|
+      roman_numeral << letter * (arabic_number / number)
+      arabic_number = arabic_number % number
+    end
+    roman_numeral
+  end
 end
--- roman_numerals/spec/roman_numerals_spec.rb
+++ roman_numerals/spec/roman_numerals_spec.rb
@@ -4,4 +4,31 @@ describe RomanNumerals do
   it "should be able to convert 1 to I" do
     expect(RomanNumerals.convert(1)).to eq("I")
   end
+
+  it "should be able to convert 5 to V" do
+    expect(RomanNumerals.convert(5)).to eq("V")
+  end
+
+  it "should be able to convert 10 to X" do
+    expect(RomanNumerals.convert(10)).to eq("X")
+  end
+
+  it "should be able to convert 35 to XXXV" do
+    expect(RomanNumerals.convert(35)).to eq("XXXV")
+  end
+
+  it "should be able to convert 93 to XCIII" do
+    expect(RomanNumerals.convert(93)).to eq("XCIII")
+  end
+
+  it "should be able to convert 141 to CXLI" do
+    expect(RomanNumerals.convert(141)).to eq("CXLI")
+  end
+
+  it "should be able to convert 575 to DLXXV" do
+    expect(RomanNumerals.convert(575)).to eq("DLXXV")
+  end
+  it "should be able to convert 911 to CMXI" do
+    expect(RomanNumerals.convert(911)).to eq("CMXI")
+  end
 end
